d2e3ecb311adc1937dcc172bdf225b6de9f85b33
pixelomer <37103802+pixelomer@users.noreply.github.com>
2022-02-04 11:36:15 +0300

    Throw the fuse-bindings error only if require() fails

--- src/anyfs/fuse.ts
+++ src/anyfs/fuse.ts
@@ -7,11 +7,12 @@ export interface AnyFSMountOptions {
 }
 
 export async function fuseMount(FS: AnyFS, mountPoint: string, options?: AnyFSMountOptions, onDestroy?: () => void) {
+	let fuse: any;
 	try {
-		const fuse = require("./_fuse");
-		return fuse.fuseMount(FS, mountPoint, options, onDestroy);
+		fuse = require("./_fuse");
 	}
 	catch {
 		throw new Error("The fuse-bindings package is missing.");
 	}
+	return fuse.fuseMount(FS, mountPoint, options, onDestroy);
 }
