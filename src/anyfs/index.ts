68fc3e0de797a25465ff599f8b5bbd689ad1f5e6
pixelomer <37103802+pixelomer@users.noreply.github.com>
2022-02-23 19:29:57 +0300

    Use "export {} from" syntax

--- src/anyfs/index.ts
+++ src/anyfs/index.ts
@@ -1,6 +1,4 @@
-import { AnyFS } from "./anyfs";
-import { AnyFSFile } from "./fs-file";
-import { AnyFSFolder } from "./fs-folder";
-import { AnyFSProvider } from "./provider";
-
-export { AnyFS, AnyFSFile, AnyFSFolder, AnyFSProvider };
+export { AnyFS } from "./anyfs";
+export { AnyFSFile } from "./fs-file";
+export { AnyFSFolder } from "./fs-folder";
+export { AnyFSProvider } from "./provider";
